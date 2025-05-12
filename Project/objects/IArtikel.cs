8f436618ad30c81b1c5cc276cb9e665371ef6551
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2025-05-12 10:20:55 +0200

    [docs] Exported ER-Diagram for Documentation | Files: PSS_ER_Modell.png, Project/MVVM/View/ArtikelView.xaml, Project/MVVM/View/HomeView.xaml, Project/objects/IArtikel.cs

--- Project/objects/IArtikel.cs
+++ Project/objects/IArtikel.cs
@@ -4,12 +4,34 @@ namespace Project.objects;
 
 public interface IArtikel
 {
+    /// <summary>
+    /// Atribute for the ID of the Article
+    /// </summary>
     public int ID { get; }
+    /// <summary>
+    /// Atribute for the name of the Article
+    /// </summary>
     public string name { get; }
+    /// <summary>
+    /// Atribute for the description of the Article
+    /// </summary>
     public string description { get; }
+    /// <summary>
+    /// Atribute for the type of unit of the Article
+    /// <example>Kilograms / Liters / Meters</example>
+    /// </summary>
     public string unit { get; }
+    /// <summary>
+    /// Atribute for the count of how many Articles of that type you have
+    /// </summary>
     public int StockQuantity { get; }
+    /// <summary>
+    /// Atribute for the Creation time
+    /// </summary>
     public DateTime timestamp { get; }
 
+    /// <summary>
+    /// Function for Updating the Article
+    /// </summary>
     public virtual void Update() {}
 }
