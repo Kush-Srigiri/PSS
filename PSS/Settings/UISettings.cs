0d8c2099a0e048f844e35b810c5b80f41ff5cc76
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2025-05-26 11:31:27 +0200

    [feature] Added Darkmode correct and Complete summery of Artikel | Files: PSS/MVVM/View/ArtikelView.xaml, PSS/MVVM/View/HomeView.xaml, PSS/MVVM/ViewModel/HomeViewModel.cs, PSS/Settings/ThemeManager.cs, PSS/Settings/UISettings.cs, PSS/assets/Colors/Darkmode.xaml, PSS/assets/Colors/Lightmode.xaml, PSS/assets/Themes/ListItem.xaml, PSS/objects/Items/ArtikelListItem.cs, __tests__/main.cs

--- PSS/Settings/UISettings.cs
+++ PSS/Settings/UISettings.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Configuration;
-using Microsoft.Windows.Themes;
 
 namespace PSS.Settings
 {
@@ -18,31 +18,65 @@ namespace PSS.Settings
         System
     }
 
-    class UISettings : ConfigurationSection
+    class UISettings : ConfigurationSection, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
         [ConfigurationProperty("darkTheme", DefaultValue = (int)ColorThemeOption.System)]
         public int DarkThemeOption
         {
             get => (int)this["darkTheme"];
-            set => this["darkTheme"] = value;
+            set
+            {
+                if ((int)this["darkTheme"] != value)
+                {
+                    this["darkTheme"] = value;
+                    OnPropertyChanged(nameof(DarkTheme));
+                }
+            }
         }
+
         public ColorThemeOption DarkTheme
         {
             get => (ColorThemeOption)DarkThemeOption;
-            set => DarkThemeOption = (int)value;
+            set
+            {
+                if (DarkThemeOption != (int)value)
+                {
+                    DarkThemeOption = (int)value;
+                    OnPropertyChanged(nameof(DarkTheme));
+                }
+            }
         }
 
         [ConfigurationProperty("iconTheme", DefaultValue = (int)ThemeOption.Normal)]
         public int SelectedOption
         {
             get => (int)this["iconTheme"];
-            set => this["iconTheme"] = value;
+            set
+            {
+                if ((int)this["iconTheme"] != value)
+                {
+                    this["iconTheme"] = value;
+                    OnPropertyChanged(nameof(SelectedTheme));
+                }
+            }
         }
 
         public ThemeOption SelectedTheme
         {
             get => (ThemeOption)SelectedOption;
-            set => SelectedOption = (int)value;
+            set
+            {
+                if (SelectedOption != (int)value)
+                {
+                    SelectedOption = (int)value;
+                    OnPropertyChanged(nameof(SelectedTheme));
+                }
+            }
         }
     }
 }
--- PSS/objects/Items/ArtikelListItem.cs
+++ PSS/objects/Items/ArtikelListItem.cs
@@ -6,9 +6,15 @@ namespace PSS.objects.Items;
 public class ArtikelListItem
 {
     public string title { get; set; }
-
-    public ArtikelListItem(string title)
+    public string description { get; set; }
+    public string unit { get; set; }
+    public int stockQuantity { get; set; }
+    
+    public ArtikelListItem(Artikel art)
     {
-        this.title = title;
+        this.title = art.name;
+        this.description = art.description;
+        this.unit = art.unit;
+        this.stockQuantity = art.StockQuantity;
     }
 }
